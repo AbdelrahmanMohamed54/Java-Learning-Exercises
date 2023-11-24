3b99a5c70e07420b2d7b6c188604d401120649e3
AbdelrahmanMohamed54 <budimohamed572@gmail.com>
2023-11-24 20:51:18 +0100

    My first Java program, Hello, World!

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print the greeting message to the console.
+        System.out.println("Hello, World!");
+    }
+}
