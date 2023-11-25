387ca9f51ffc1d0f9b12468ddb5ffc57101a5fc7
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-11-25 21:43:51 +0100

    String manipulation java exercise

---
+++ Variables_And_Data_Types_Exercises/String_Manipulation.java
@@ -0,0 +1,27 @@
+package Variables_And_Data_Types_Exercises;
+public class String_Manipulation {
+    public static void main(String[] args) {
+        // Declare strings
+        String firstName = "John";
+        String lastName = "Doe";
+
+        // Concatenate strings
+        String fullName = firstName + " " + lastName;
+
+        // Explore string manipulation
+        int length = fullName.length();
+        String upperCaseFullName = fullName.toUpperCase();
+        String lowerCaseFullName = fullName.toLowerCase();
+        char firstChar = fullName.charAt(0);
+
+        // Print the results
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Length: " + length);
+        System.out.println("Upper Case: " + upperCaseFullName);
+        System.out.println("Lower Case: " + lowerCaseFullName);
+        System.out.println("First Character: " + firstChar);
+    }
+}
+
+
+// In the code above I explored string Manipulation by concatinating strings using +
