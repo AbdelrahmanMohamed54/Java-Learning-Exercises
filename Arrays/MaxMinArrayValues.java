75a3a38c8a6509be176ed9e40f1ff5ae1c530954
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-11-30 18:51:42 +0100

    Java program to find the maximum and minimum values in an array.

---
+++ Arrays/MaxMinArrayValues.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MaxMinArrayValues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+        int min = array[0];
+
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        System.out.println("Maximum value: " + max);
+        System.out.println("Minimum value: " + min);
+
+        scanner.close();
+    }
+}
