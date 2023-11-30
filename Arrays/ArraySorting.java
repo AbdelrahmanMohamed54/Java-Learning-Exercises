ccf28a2c11d7e155e5a945500d0fac136c00cbb7
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-11-30 18:55:34 +0100

    Implement Java program using the quicksort algorithm to sort an array of integers in ascending order.

---
+++ Arrays/ArraySorting.java
@@ -0,0 +1,63 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        // Sorting the array using a sorting algorithm (e.g., quicksort)
+        quickSort(array, 0, size - 1);
+
+        System.out.println("Sorted array:");
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+
+        scanner.close();
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+
+            // Recursively sort elements before and after the pivot
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+
+                // Swap array[i] and array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Swap array[i+1] and array[high] (pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+}
