0810f9a16aca5cebff778b8b6aeaf7fa9ecd9852
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-12-01 09:14:58 +0100

    Implement palindrome checker function for strings, ensuring efficient and accurate verification.

---
+++ MethodsAndFunctions/PalindromeChecker.java
@@ -0,0 +1,36 @@
+package MethodsAndFunctions;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to check if it's a palindrome: ");
+        String inputString = scanner.nextLine();
+
+        if (isPalindrome(inputString)) {
+            System.out.println("The entered string is a palindrome.");
+        } else {
+            System.out.println("The entered string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
