657f65a89aa56a818f60b2f2e9bd8ae535ef6b24
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-12-05 10:51:37 +0100

    Add Custom Exception for Divide By Zero Handling

---
+++ ExceptionHandling/FileExceptionHandling/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package ExceptionHandling.FileExceptionHandling;
+
+public class DivideByZeroException extends Exception {
+    public DivideByZeroException(String message) {
+        super(message);
+    }
+}
+
