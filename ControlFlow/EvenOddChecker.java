6a56eed0a60df825cf1c68e2135a0074cd52d6a4
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-12-05 10:57:46 +0100

    remove _ from folder name to maintain naming best practices

---
+++ ControlFlow/EvenOddChecker.java
@@ -0,0 +1,20 @@
+package ControlFlow;
+
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        scanner.close();
+    }
+}
