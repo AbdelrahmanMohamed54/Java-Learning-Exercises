e86eb46cce00829749b53b37ce04b31e684d5820
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-12-07 08:21:33 +0100

    Remove _ from file name to maintain best practices

---
+++ VariablesAndDataTypesExercises/Type_Conversion.java
@@ -0,0 +1,19 @@
+package Variables_And_Data_Types_Exercises;
+public class Type_Conversion {
+    public static void main(String[] args) {
+        // Convert int to double
+        int intValue = 10;
+        double doubleValue = (double) intValue;
+
+        // Convert double to int
+        double anotherDoubleValue = 15.75;
+        int anotherIntValue = (int) anotherDoubleValue;
+
+        // Print the results
+        System.out.println("Converted int to double: " + doubleValue);
+        System.out.println("Converted double to int: " + anotherIntValue);
+    }
+}
+
+
+// Explore type conversion by converting variables from one data type to another. In this example above, I converted an int to a double and vice versa.
