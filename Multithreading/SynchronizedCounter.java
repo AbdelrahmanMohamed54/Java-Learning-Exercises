821de67cfeb441a075062c06ba397623b791c25b
AbdelrahmanMohamed54 <125618672+AbdelrahmanMohamed54@users.noreply.github.com>
2023-12-11 17:52:51 +0100

    Implement SynchronizedCounter program to avoid race conditions

---
+++ Multithreading/SynchronizedCounter.java
@@ -0,0 +1,45 @@
+package Multithreading;
+
+public class SynchronizedCounter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SynchronizedCounter counter = new SynchronizedCounter();
+
+        // Create and start two threads
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        // Wait for threads to complete
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Display the final count
+        System.out.println("Final Count: " + counter.getCount());
+    }
+}
+
